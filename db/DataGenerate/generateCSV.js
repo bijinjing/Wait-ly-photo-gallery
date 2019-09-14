38255aed8cdaa2646434f40aada4a27384bece2b
Jinjing <bijinjing88@gmail.com>
2019-09-13 21:05:40 -0700

    update the csv generation file

--- db/DataGenerate/generateCSV.js
+++ db/DataGenerate/generateCSV.js
@@ -1,13 +1,37 @@
 const fs = require('fs');
 const faker = require('faker');
 
+const gallaryGenerator = (restaurantId,imageId) => {
 
-const write = (writer) => {
+  let image_id = (restaurantId-1) * 10 + imageId;
+  let index = Math.floor(Math.random() * 9001);
+  let url = `https://jinjing-photo-gallery.s3-us-west-1.amazonaws.com/photos/${index}.jpg`;
+  let description = faker.lorem.words();
+      description = description.split(' ');
+      description = description.map(word => word[0].toUpperCase() + word.substr(1));
+      description = description.join(' ');
+  let user_submit = faker.random.boolean();
+  let date = faker.date.past();
+
+  return `${image_id}, ${url}, ${description}, ${user_submit}, ${date}`
+}
+
+const restaurantGenerator = (restaurantId) => {
+  let id = restaurantId;
+  let restaurant_name = faker.lorem.words();
+  let record = '';
+  for (var i=1; i<=10; i++) {
+    let imageDetail = gallaryGenerator(restaurantId,i);
+    let record += `${id},${restaurant_name},${imageDetail}\n`
+  }
+  return record;
+}
+
+const write = (writer,data) => {
   return new Promise((resolve) => {
     if (!writer.write(data)) {
       writer.once('drain', resolve)
-    }
-    else {
+    } else {
       resolve()
     }
   })
@@ -15,7 +39,10 @@ const write = (writer) => {
 
 const generator = async(path, num, iterator) => {
   let writelistings = fs.createWriteStream(path);
-  let readinglisting = fs.read
-      await write(writelistings, iterator(i))
+  for(let i=1; i<=num; i++){
+    await write(writelistings, iterator(i))
+  }
   await console.log('complete',path)
 }
+
+generator(./test2.cvs,10,restaurantGenerator)
