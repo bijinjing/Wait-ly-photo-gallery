e029f03a8375be14d446a3e9182919dd9ef40fa0
Jinjing <bijinjing88@gmail.com>
2019-09-27 12:12:58 -0700

    update for redish

--- server/index.js
+++ server/index.js
@@ -6,20 +6,20 @@ const db = require('../db/index.js');
 const dbCassandra = require('../db/DataQuery/cassandraInquery.js');
 const dbPostgres = require('../db/DataQuery/postgresInquery.js');
 const uuidv4 = require('uuid/v4');
-// const redis = require('redis')
-// const client = redis.createClient(6379, '52.8.132.146');
-// client.auth('Student111!')
+const redis = require('redis')
+const client = redis.createClient(6379, '52.8.132.146');
+client.auth('Student111!')
 
-// const goRedis = (req, res, next) => {
-//   client.get(req.params.listing, (err, reply) => {
-//     if (err) throw err;
-//     if (reply !== null) {
-//       res.send(JSON.parse(reply));
-//     } else {
-//       next();
-//     }
-//   });
-// };
+const goRedis = (req, res, next) => {
+  client.get(req.params.listing, (err, reply) => {
+    if (err) throw err;
+    if (reply !== null) {
+      res.send(JSON.parse(reply));
+    } else {
+      next();
+    }
+  });
+};
 
 
 const app = express();
@@ -39,7 +39,7 @@ app.use(function(req, res, next) {
 });
 
 //get photo gallaries
-app.get('/api/restaurants/:listing', (req, res) => {
+app.get('/api/restaurants/:listing', goRedis, (req, res) => {
   
   let params = req.params.listing;
   // db.getImagesFromListing(params, (error, images) => {
